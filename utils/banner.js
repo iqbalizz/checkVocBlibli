4ef942001bfeaa4a2e5684cabe40fb87bdddfa13
iqbalizz <iqbalizzudin13@gmail.com>
2023-09-06 21:08:18 +0700

    Add banner.js

---
+++ utils/banner.js
@@ -0,0 +1,17 @@
+export const banner1 = () => {
+    const valueBanner = `
+==========================================
+         DATA PROFILE ACCOUNT
+==========================================
+    `;
+    return valueBanner
+}
+
+export const banner2 = () => {
+    const valueBanner = `
+==========================================
+           VOUCHER TERSEDIA
+==========================================
+    `;
+    return valueBanner
+}
